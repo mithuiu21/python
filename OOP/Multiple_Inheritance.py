0ba4219421c776f8c60a943791bbf694701bf6b6
Kamruzzaman Mithu <41517436+mithuiu21@users.noreply.github.com>
2023-09-20 21:52:18 +0600

    Multiple Inheritance concept

---
+++ OOP/Multiple_Inheritance.py
@@ -0,0 +1,24 @@
+class Father():
+    def gardening(self):
+        print("I enjoy gardening")
+    def skills():
+        print("programming")
+
+class Mother():
+    def cooking(self):
+        print("I love cooking")
+    def skills():
+        print("art")
+
+class Child(Father, Mother):
+    def sports(self):
+        Father.skills()
+        Mother.skills()
+        print("I enjoy sports")
+
+
+c = Child()
+c.gardening()
+c.cooking()
+c.sports()
+
