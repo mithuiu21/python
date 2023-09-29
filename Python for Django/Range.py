1a8bc5b72221a7d2ed1f8599a619687aaae3a951
Kamruzzaman Mithu <41517436+mithuiu21@users.noreply.github.com>
2023-09-29 19:00:54 +0600

    Range Basics

---
+++ Python for Django/Range.py
@@ -0,0 +1,16 @@
+# Range
+# range(start, stop, step)
+# range(1, 10, 2)
+
+# Create a list with range
+a = list(range(10)) #(0-9)
+print(a)
+
+b = list(range(1, 10, 2)) # Odd number
+print(b)
+
+c = list(range(-10, 0, 2))
+print(c)
+
+for i in range(2, 10, 2):
+    print(i)
