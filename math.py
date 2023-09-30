36fabfe01dbe28aa5a2aa09fc7b08d8dd9a0d1c2
Kamruzzaman Mithu <41517436+mithuiu21@users.noreply.github.com>
2023-09-30 18:35:01 +0600

    Py math built-in function

---
+++ math.py
@@ -0,0 +1,36 @@
+## Built-in math function
+
+x = min(1, 2, 3)
+y = max(1, 2, 3)
+
+print(x)
+print(y)
+
+## abs() : absolute(positive)
+z = abs(-11.99)
+print(z)
+
+# pow()
+p = pow(3, 3)
+print(p)
+
+## The math module
+
+import math
+
+x = math.sqrt(64)
+print(x)
+
+# rounds a number upwards to its nearest integer
+# rounds a number downwards to its nearest integer
+c = math.ceil(1.7)
+f = math.floor(1.7)
+
+print(c)
+print(f)
+
+# PI
+
+PI = math.pi 
+print(PI)
+
